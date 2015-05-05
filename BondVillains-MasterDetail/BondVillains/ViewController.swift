a33f2eee8e3009d3a5201f5d5cb47c8dea5afc2a
GabrielleM <udacity@udacitys-MacBook-Air-2.local>
2015-05-04 19:22:54 -0700

    another Swift 1.2 change

--- BondVillains-MasterDetail/BondVillains/ViewController.swift
+++ BondVillains-MasterDetail/BondVillains/ViewController.swift
@@ -40,7 +40,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
-        let detailController = self.storyboard!.instantiateViewControllerWithIdentifier("VillainDetailViewController")! as! VillainDetailViewController
+        let detailController = self.storyboard!.instantiateViewControllerWithIdentifier("VillainDetailViewController") as! VillainDetailViewController
         detailController.villain = self.allVillains[indexPath.row]
         self.navigationController!.pushViewController(detailController, animated: true)
     
