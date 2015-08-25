3043c67acb399826b75fc25f9a5ea29151dd24cb
Ten Mutunhire <tmutunhire@gmail.com>
2015-08-25 18:34:25 +0200

    a nice adventure app and navigation

---
+++ TheTiger/TheTiger/MYOAViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MYOAViewController.swift
+//  TheTiger
+//
+//  Created by Tomahawk Africa Tindo on 8/25/15.
+//  Copyright (c) 2015 Udacity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MYOAViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Start Over", style: .Plain, target: self, action: "startOver")
+        
+    }
+    
+    func startOver() {
+        if let navigationController = self.navigationController {
+            navigationController.popToRootViewControllerAnimated(true)
+        }
+    }
+}
